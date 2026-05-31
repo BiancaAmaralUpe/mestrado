d9805b1628ca7c7b8ea24c232aa171fc65f77948
Bianca <bianca.amaral@upe.br>
2026-05-31 16:15:53 -0300

    subida dos hiperparametros

---
+++ Inteligencia_computacional/pratica_mlp/constants.py
@@ -0,0 +1,12 @@
+# ========================================================================================
+# Hiperparâmetros principais.
+# ========================================================================================
+random_seed = 42
+quantidade_pixels_imagem = 28 * 28
+quantidade_neuronios_ocultos = 100
+quantidade_digitos = 10
+quantidade_epocas = 20
+tamanho_batch = 4096
+taxa_aprendizado = 0.001
+percentual_validacao = 0.10
+caminho_checkpoint = "checkpoints/best_mlp.pth"
